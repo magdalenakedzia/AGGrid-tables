914f7b4eca3c5d7818629756fe95ef00d6375029
Magdalena Kędzia <magdalena.kedzia3@gmail.com>
2022-09-22 22:31:29 +0200

    add typescript, eslint, prettier, implement home page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HomePage } from './pages/HomePage';
+
+const App = (): JSX.Element => {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./index.scss";
+
+const HomePage = (): JSX.Element => {
+    return (
+        <>
+            <h1>AG GRID TABLES</h1>
+            <button className="btn first"> Table </button>
+            <button className="btn second"> Table </button>
+            <button className="btn third"> Table </button>
+            <button className="btn fourth"> Table </button>
+            <button className="btn fifth"> Table </button>
+            <button className="btn sixth"> Table </button>
+        </>
+    )
+}
+
+export default HomePage;
