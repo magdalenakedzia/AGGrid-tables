4700274251fd285051fd85f270409313a7d1bf3a
Magdalena Kędzia <magdalena.kedzia3@gmail.com>
2022-09-23 00:08:55 +0200

    add router

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import {
+    BrowserRouter as Router,
+    Link
+  } from "react-router-dom";
 import "./index.scss";
 
 const HomePage = (): JSX.Element => {
     return (
         <>
             <h1>AG GRID TABLES</h1>
-            <button className="btn first"> Table </button>
-            <button className="btn second"> Table </button>
-            <button className="btn third"> Table </button>
-            <button className="btn fourth"> Table </button>
-            <button className="btn fifth"> Table </button>
-            <button className="btn sixth"> Table </button>
+            <div className='buttons-container'>
+            <Router>
+                <Link to="/table-client-side" className="btn first"> Table </Link>
+                <Link  to="/" className="btn second"> Table </Link>
+                <Link  to="/" className="btn third"> Table </Link>
+                <Link  to="/" className="btn fourth"> Table </Link>
+                <Link  to="/" className="btn fifth"> Table </Link>
+                <Link  to="/" className="btn sixth"> Table </Link>
+                </Router>
+            </div>
         </>
     )
 }
